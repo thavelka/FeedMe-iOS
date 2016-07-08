0e324a35bb75fa231b4727d360ad6cbb52b94dad
thavelka <tim.havelka@gmail.com>
2016-07-07 23:20:03 -0500

    Added main storyboard and View Controllers.

---
+++ FeedMe/Views/Cells/ListingCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListingCell.swift
+//  FeedMe
+//
+//  Created by Tim Havelka on 7/7/16.
+//  Copyright © 2016 Tim Havelka. All rights reserved.
+//
+
+import UIKit
+
+class ListingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
